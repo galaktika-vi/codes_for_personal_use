b7f0245047d0f35a7bcfaaa0690cef38ac18fa9b
Viktoriia Shustrova <129378571+galaktika-vi@users.noreply.github.com>
2023-05-30 17:53:37 +0200

    Update CookieGenerator.java

--- CookieGenerator.java
+++ CookieGenerator.java
@@ -14,6 +14,12 @@ public class CookieGenerator {
             sites.add(input);
         }
 
+        System.out.println("Введите логин: ");
+        String login = scanner.nextLine();
+
+        System.out.println("Введите пароль: ");
+        String password = scanner.nextLine();
+
         System.out.println("Введите количество файлов с куками для генерации: ");
         int numFiles = scanner.nextInt();
 
@@ -24,40 +30,57 @@ public class CookieGenerator {
         String netscapeFolder = mainFolder + File.separator + "Netscape";
 
         for (int i = 0; i < numFiles; i++) {
-            generateCookies(sites, i, jsonFolder, netscapeFolder);
+            generateCookies(sites, i, jsonFolder, netscapeFolder, login, password);
         }
     }
 
-    private static void generateCookies(List<String> sites, int fileNum, String jsonFolder, String netscapeFolder) {
+    private static void generateCookies(List<String> sites, int fileNum, String jsonFolder, String netscapeFolder, String login, String password) {
         new File(jsonFolder).mkdirs();
         new File(netscapeFolder).mkdirs();
 
-        String jsonFileName = jsonFolder + File.separator + fileNum + ".json";
+        String jsonFileName = jsonFolder + File.separator + fileNum + ".txt";
         String netscapeFileName = netscapeFolder + File.separator + fileNum + ".txt";
 
         for (String site : sites) {
-            try (FileWriter jsonWriter = new FileWriter(jsonFileName, true);
+            try (PrintWriter jsonWriter = new PrintWriter(new BufferedWriter(new FileWriter(jsonFileName, true)));
                  FileWriter netscapeWriter = new FileWriter(netscapeFileName, true)) {
 
-                jsonWriter.write("[\n");
+                jsonWriter.println("[");
                 for (int i = 0; i < 5; i++) {
                     String cookieName = "cookie" + i;
                     String cookieValue = UUID.randomUUID().toString();
                     long currentTime = System.currentTimeMillis();
 
-                    jsonWriter.write("  {\n");
-                    jsonWriter.write("    \"domain\": \"" + site + "\",\n");
-                    jsonWriter.write("    \"name\": \"" + cookieName + "\",\n");
-                    jsonWriter.write("    \"value\": \"" + cookieValue + "\",\n");
-                    jsonWriter.write("    \"expirationDate\": " + (currentTime + 86400000) + "\n");
-                    jsonWriter.write(i < 4 ? "  },\n" : "  }\n");
+                    jsonWriter.println("  {");
+                    jsonWriter.println("    \"domain\": \"" + site + "\",");
+                    jsonWriter.println("    \"name\": \"" + cookieName + "\",");
+                    jsonWriter.println("    \"value\": \"" + cookieValue + "\",");
+                    jsonWriter.println("    \"expirationDate\": " + (currentTime + 86400000));
+                    jsonWriter.println(i < 4 ? "  }," : "  }");
 
-                    netscapeWriter.write(site + "\tFALSE\t/\tFALSE\t" + (currentTime + 86400000) + "\t" + cookieName + "\t" + cookieValue + "\n");
+                    netscapeWriter.write(site + "\tTRUE\t/\tFALSE\t" + (currentTime + 86400000) + "\t" + cookieName + "\t" + cookieValue + "\n");
                 }
-                jsonWriter.write("]");
+
+                // Добавляем куки для логина и пароля
+                String cookieLogin = "login";
+                String cookiePassword = "password";
+
+                                jsonWriter.println("  {");
+                jsonWriter.println("    \"domain\": \"" + site + "\",");
+                jsonWriter.println("    \"name\": \"" + cookiePassword + "\",");
+                jsonWriter.println("    \"value\": \"" + password + "\",");
+                jsonWriter.println("    \"expirationDate\": " + (System.currentTimeMillis() + 86400000));
+                jsonWriter.println("  }");
+
+                jsonWriter.println("]");
+
+                netscapeWriter.write(site + "\tTRUE\t/\tFALSE\t" + (System.currentTimeMillis() + 86400000) + "\t" + cookieLogin + "\t" + login + "\n");
+                netscapeWriter.write(site + "\tTRUE\t/\tFALSE\t" + (System.currentTimeMillis() + 86400000) + "\t" + cookiePassword + "\t" + password + "\n");
+
             } catch (IOException e) {
                 e.printStackTrace();
             }
         }
     }
 }
+
